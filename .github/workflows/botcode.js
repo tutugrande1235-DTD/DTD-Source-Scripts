f5370ac3da953314e0a92cf5acc936defc5d1c5e
tutugrande1235-DTD <jr16102022@gmail.com>
2026-02-10 21:51:59 -0400

    Atualizar o botcode.js

--- .github/workflows/botcode.js
+++ .github/workflows/botcode.js
@@ -1,22 +1,19 @@
+const { execSync } = require("child_process")
+
 const owner = "tutugrande1235"
 const repo = "tutugrande1235-DTD"
 const issue_number = process.env.ISSUE_NUMBER
 const comment = process.env.COMMENT_BODY
 const token = process.env.GITHUB_TOKEN
 
-let reply = ""
 const cbody = comment.split("@")
 const user = cbody[0] || "User"
 const to = cbody[1] || "User"
 const c = cbody[2] || "nil"
 
-if (!c || !user || !to){
-  process.exit(0)
-}
+if (!c || !user || !to) process.exit(0)
 
-console.log("user:", user)
-console.log("body:", c)
-
+let reply = ""
 if (c.includes("test")) {
   reply = `dont test me @${user}!`
 } else if (c.includes("bad")) {
@@ -25,9 +22,7 @@ if (c.includes("test")) {
 
 if (reply) {
   const form = `System@${user}@${reply}`
-  const { execSync } = require("child_process")
   const cmd = `curl -s -X POST -H "Authorization: token ${token}" -H "User-Agent: DOT-Bot" -d '{"body":"${form}"}' https://api.github.com/repos/${owner}/${repo}/issues/${issue_number}/comments`
   console.log("Executando:", cmd)
-  const result = execSync(cmd).toString()
-  console.log(result)
+  console.log(execSync(cmd).toString())
 }
