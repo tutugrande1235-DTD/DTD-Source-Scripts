98ed1319bb8d6edccf561f634bbae581c4ab5274
tutugrande1235-DTD <jr16102022@gmail.com>
2026-02-10 21:00:37 -0400

    Atualizar o botcode.js

--- .github/workflows/botcode.js
+++ .github/workflows/botcode.js
@@ -1,15 +1,16 @@
 async function main(){
 const owner = "tutugrande1235"
 const repo = "tutugrande1235-DTD"
-const issue_number = 1
+const issue_number = 7
 const comment = context.payload.comment.body
 let reply = ""
 const cbody = comment.split("@")
 const user = cbody[0]
+const to = cbody[1]
 let form = ""
-const c = cbody[1]
+const c = cbody[2]
 
-if (!c || !user){
+if (!c || !user || !to){
   return
 }
 
@@ -17,14 +18,12 @@ console.log("user: " + user + "\nbody: " + c)
 
 if (c.includes("test")){
   reply = `dont test me @${user}!`
-} else if (!user) {
-  reply = `@${user} commented on the issue without use the app!`
 } else if (c.includes("bad") && c.includes("package")) {
   reply = `@${user} dont be SUCK!`
 }
 
 if (reply){
-  form = `System@${reply}`
+  form = `System@${user}@${reply}`
 }
 
 await github.issues.createComment({
