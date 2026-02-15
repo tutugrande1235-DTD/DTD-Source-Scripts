8f3aa87a7d08961a4853b2b8f42388975a5bedce
Dougla037 <nathanc0dxxx@gmail.com>
2026-02-15 13:45:13 -0400

    Atualizar o accountservicecode.lua

--- .github/workflows/accountservicecode.lua
+++ .github/workflows/accountservicecode.lua
@@ -64,3 +64,12 @@ do
         return
     end
 end
+
+print("cleaning terminal logs...")
+for i = 1,200 do
+    io.write("\27[1A")
+end
+for i = 1,200 do
+    print("\rLINE REMOVED: #########################################")
+end
+print("cleaned!")
